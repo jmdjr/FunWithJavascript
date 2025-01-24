aa8d0190bb4dbd4457d373585c8294faa9b7ff04
jmdjr <jmdjr@users.noreply.github.com>
2025-01-24 19:40:17 +0000

    deploy: 816883ee2e6f92e2c0c48fb028538e361f128922

--- assets/site.data.ts
+++ assets/site.data.ts
@@ -2,4 +2,8 @@ export const HEADING =
   `Fun with Javascript`;
 
 export const DESCRIPTION = 
-`Work I did playing with javascript, jquery and the like to build some dinky tools and games.`
+`Work I did playing with javascript, jquery and the like to show I could, and to have a little fun with Font Awesome.<br/><br/>
+The main bulk of logic for these components is in javascript, with an access layer built in Angular (as with most of my modern projects sites)
+<br/>
+<br/>
+click a card below to see them in action!`
